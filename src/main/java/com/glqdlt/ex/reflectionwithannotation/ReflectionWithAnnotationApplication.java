a6a5c25d9be5601b44fb75a5d4659acde00d31d8
glqdlt <dlfdnd0725@gmail.com>
2019-02-19 21:12:54 +0900

    init

---
+++ src/main/java/com/glqdlt/ex/reflectionwithannotation/ReflectionWithAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.glqdlt.ex.reflectionwithannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflectionWithAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReflectionWithAnnotationApplication.class, args);
+	}
+
+}
